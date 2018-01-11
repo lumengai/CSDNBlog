71403f17b17d54947db0b9cea593d40595bb2d93
unknown <1280227259@qq.com>
2018-01-11 11:46:00 +0800

    first commit

---
+++ CSDNBlog/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSDNBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 博客
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 学院 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult College()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 下载
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Download()
+        {
+            return View();
+        }
+
+    }
+}
